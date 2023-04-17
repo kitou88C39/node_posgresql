cbf60d88a3ec3c6fffa01506e9d3cd7aa1b75482
kitou88C39 <kanfutrooper@icloud.com>
2023-04-17 21:48:44 +0900

    server.jsの編集

--- server.js
+++ server.js
@@ -20,10 +20,34 @@ app.get('/users', (req, res) => {
 //特定のUserを取得する
 app.get('/users/:id', (req, res) => {
   const id = req.params.id;
+
   pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
     if (error) throw error;
     return res.status(200).json(results.row);
   });
+});
+
+//userを追加する
+app.post('/users', (req, res) => {
+  const { name, email, age } = req.body;
+  //userが既に存在しているかどうか確認
+  pool.query(
+    'SELECT s FROM users s WHERE s.email = $1',
+    [email],
+    (error, results) => {
+      if (results.rows.length) {
+        res.send('既にユーザーが存在しています');
+      }
+      pool.query(
+        'INSERT INTO users(name, email, age) values($1,$2,$3)',
+        [name, email, age],
+        (error, results) => {
+          if (error) throw error;
+          res.status(201).send('ユーザー作成に成功しました');
+        }
+      );
+    }
+  );
 });
 
 app.listen(PORT, () => {
